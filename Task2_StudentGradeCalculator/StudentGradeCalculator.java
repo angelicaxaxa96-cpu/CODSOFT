8ef16802ceff0ba14195bd6c82b82e0adc389d83
angelicaxaxa96-cpu <angelicaxaxa96@gmail.com>
2026-06-23 22:56:51 +0530

    Update StudentGradeCalculator.java

--- Task2_StudentGradeCalculator/StudentGradeCalculator.java
+++ Task2_StudentGradeCalculator/StudentGradeCalculator.java
@@ -1,1 +1,33 @@
+// STUDENT GRADE CALCULATOR
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+        String grade;
+        System.out.println("Enter the marks of 5 each subject");
+        for (int i = 0; i < 5; i++) {
+            int marks = sc.nextInt();
+            total = total + marks;
+        }
+        double avg = (double) total / 5;
+        if (avg >= 90)
+            grade = "A+";
+        else if (avg >= 80)
+            grade = "A";
+        else if (avg >= 65)
+            grade = "B";
+        else if (avg >= 50)
+            grade = "C";
+        else if (avg >= 33)
+            grade = "D";
+        else
+            grade = "F";
+        System.out.println("Total marks: " + total);
+        System.out.println("Averaage Percentage: " + avg);
+        System.out.println("Grade: " + grade);
+    }
+}
+
 
